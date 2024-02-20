4027c95c0c8117a798c56834830ed4776e9e1cbb
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-02-20 12:17:06 +0100

    data lake + formatting

---
+++ 03_database.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE adv_data;
+USE adv_data;
+
+CREATE TABLE dati (
+	adv_ID INT, 
+	Radio FLOAT, 
+	Newspaper FLOAT, 
+	Actress VARCHAR(5), 
+	Social INT, 
+	tv_fatturato INT, 
+	radio_fatturato INT, 
+	social_fatturato INT, 
+	paper_fatturato INT, 
+	Sales INT, 
+	TV FLOAT, 
+	Actor VARCHAR(5)
+)
+
